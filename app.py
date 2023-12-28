35adfa10e412a5deb955814b26d85f9d3273b004
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2023-12-28 16:51:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,41 @@ st.sidebar.header("User Input")
 start_date = pd.to_datetime(st.sidebar.date_input("Select Start Date", data['Date'].min()))
 end_date = pd.to_datetime(st.sidebar.date_input("Select End Date", data['Date'].max(), data['Date'].min(), data['Date'].max()))
 
+# Specify columns for the line chart
+selected_columns = st.sidebar.multiselect(
+    "Select Columns for Line Chart",
+    ['Close', 'Points Change', 'Debt Net Investment', 'Equity Net Investment']
+)
+
+# Filter data based on user input date range
+selected_data = data[(data['Date'] >= start_date) & (data['Date'] <= end_date)]
+
+# Line chart using Plotly Express
+if not selected_data.empty and selected_columns:
+    st.plotly_chart(px.line(selected_data, x='Date', y=selected_columns, title=f'Line Chart for {start_date} to {end_date}'))
+
+# Show selected data in a table
+st.write("Selected Data:")
+st.write(selected_data)
+
+
+# Load the data from the Excel file
+file_path = 'NIFTY.xlsx'
+data = pd.read_excel(file_path)
+
+# Convert 'Date' column to Pandas Timestamp
+data['Date'] = pd.to_datetime(data['Date'])
+
+# Streamlit app
+st.title("NIFTY Data Visualization App")
+
+# Sidebar for user input
+st.sidebar.header("User Input")
+
+# Option to select start and end dates
+start_date = pd.to_datetime(st.sidebar.date_input("Select Start Date", data['Date'].min()))
+end_date = pd.to_datetime(st.sidebar.date_input("Select End Date", data['Date'].max(), data['Date'].min(), data['Date'].max()))
+
 # Option to select columns for the line chart
 selected_columns = st.sidebar.multiselect("Select Columns for Line Chart", data.columns[2:])
 
