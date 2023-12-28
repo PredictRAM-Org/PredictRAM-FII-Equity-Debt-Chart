12db184c49de02824337d25301c1783e74ad471b
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2023-12-28 16:49:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,15 +14,19 @@ st.title("NIFTY Data Visualization App")
 
 # Sidebar for user input
 st.sidebar.header("User Input")
+
+# Option to select start and end dates
 start_date = pd.to_datetime(st.sidebar.date_input("Select Start Date", data['Date'].min()))
 end_date = pd.to_datetime(st.sidebar.date_input("Select End Date", data['Date'].max(), data['Date'].min(), data['Date'].max()))
+
+# Option to select columns for the line chart
 selected_columns = st.sidebar.multiselect("Select Columns for Line Chart", data.columns[2:])
 
 # Filter data based on user input date range
-selected_data = data[(data['Date'] >= start_date) & (data['Date'] <= end_date)][['Date'] + selected_columns]
+selected_data = data[(data['Date'] >= start_date) & (data['Date'] <= end_date)]
 
 # Line chart using Plotly Express
-if not selected_data.empty:
+if not selected_data.empty and selected_columns:
     st.plotly_chart(px.line(selected_data, x='Date', y=selected_columns, title=f'Line Chart for {start_date} to {end_date}'))
 
 # Show selected data in a table
