9228bdca93cc2fe4ee224271873bd0666c8aae5b
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2023-12-28 16:42:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,15 +11,16 @@ st.title("NIFTY Data Visualization App")
 
 # Sidebar for user input
 st.sidebar.header("User Input")
-selected_date = st.sidebar.date_input("Select Date", data['Date'].min(), data['Date'].min(), data['Date'].max())
+start_date = st.sidebar.date_input("Select Start Date", data['Date'].min())
+end_date = st.sidebar.date_input("Select End Date", data['Date'].max(), data['Date'].min(), data['Date'].max())
 selected_columns = st.sidebar.multiselect("Select Columns for Line Chart", data.columns[2:])
 
-# Filter data based on user input
-selected_data = data[data['Date'] == selected_date][['Date'] + selected_columns]
+# Filter data based on user input date range
+selected_data = data[(data['Date'] >= start_date) & (data['Date'] <= end_date)][['Date'] + selected_columns]
 
 # Line chart using Plotly Express
 if not selected_data.empty:
-    st.plotly_chart(px.line(selected_data, x='Date', y=selected_columns, title=f'Line Chart for {selected_date}'))
+    st.plotly_chart(px.line(selected_data, x='Date', y=selected_columns, title=f'Line Chart for {start_date} to {end_date}'))
 
 # Show selected data in a table
 st.write("Selected Data:")
